72d8470fdf2df257085f24864f008796eee02550
Viktoryia Verpechynskis <verpechynskis@gmail.com>
2022-11-07 13:34:08 +0300

    add resource boundles and beans for i18n

--- src/main/java/com/periodicalsubscription/PeriodicalSubscriptionApplication.java
+++ src/main/java/com/periodicalsubscription/PeriodicalSubscriptionApplication.java
@@ -6,9 +6,16 @@ import com.periodicalsubscription.interceptor.MessageInterceptor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class PeriodicalSubscriptionApplication implements WebMvcConfigurer {
@@ -18,13 +25,24 @@ public class PeriodicalSubscriptionApplication implements WebMvcConfigurer {
 	}
 
 	@Bean
-	public FilterRegistrationBean<AuthorizationFilter> authorizationFilter() {
-		FilterRegistrationBean<AuthorizationFilter> registrationBean = new FilterRegistrationBean<>();
-		registrationBean.setFilter(new AuthorizationFilter());
-		registrationBean.addUrlPatterns("/user/*",
-				"/periodical/create/*", "/periodical/update/*", "/periodical/delete/*",
-				"/subscription/*", "/payment/*");
-		return registrationBean;
+	public MessageSource messageSource() {
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasename("messages");
+		return messageSource;
+	}
+
+	@Bean
+	public LocaleResolver localeResolver() {
+		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.US);
+		return localeResolver;
+	}
+
+	@Bean
+	public LocaleChangeInterceptor localeChangeInterceptor() {
+		LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+		localeChangeInterceptor.setParamName("lang");
+		return localeChangeInterceptor;
 	}
 
 	@Bean
@@ -40,6 +58,17 @@ public class PeriodicalSubscriptionApplication implements WebMvcConfigurer {
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(messageInterceptor());
-		registry.addInterceptor(loggingInterceptor()).addPathPatterns("/**");
+		registry.addInterceptor(loggingInterceptor());
+		registry.addInterceptor(localeChangeInterceptor());
+	}
+
+	@Bean
+	public FilterRegistrationBean<AuthorizationFilter> authorizationFilter() {
+		FilterRegistrationBean<AuthorizationFilter> registrationBean = new FilterRegistrationBean<>();
+		registrationBean.setFilter(new AuthorizationFilter(messageSource()));
+		registrationBean.addUrlPatterns("/user/*",
+				"/periodical/create/*", "/periodical/update/*", "/periodical/delete/*",
+				"/subscription/*", "/payment/*");
+		return registrationBean;
 	}
 }
