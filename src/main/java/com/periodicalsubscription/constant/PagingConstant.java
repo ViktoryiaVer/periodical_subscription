165da621c90286db9fba0b70914d21ecc11de30a
Viktoryia Verpechynskis <verpechynskis@gmail.com>
2022-11-27 22:56:50 +0300

    change styling and js

--- src/main/java/com/periodicalsubscription/constant/PagingConstant.java
+++ src/main/java/com/periodicalsubscription/constant/PagingConstant.java
@@ -5,6 +5,7 @@ public class PagingConstant {
     public static final int DEFAULT_PAGE_SIZE = 10;
     public static final int MAX_PAGE_SIZE = 50;
     public static final String DEFAULT_PAGE_SIZE_STRING = "10";
+    public static final String DEFAULT_PERIODICAL_PAGE_SIZE_STRING = "5";
     public static final String FIRST_PAGE_STRING = "1";
     public static final String DEFAULT_SORTING_PERIODICAL = "title";
     public static final String DEFAULT_SORTING_SUBSCRIPTION = "id";
